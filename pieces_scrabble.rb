b46b6f6393e50d9aa93abbb545e87f5b3c6fe319
Emily Chu <Emily.H.Chu@gmail.com>
2015-01-28 15:04:58 -0500

    added 7 permutations, attempted removal of proper nouns from Dictionary but failed, and added an additional test feature and modified one test feature

--- pieces_scrabble.rb
+++ pieces_scrabble.rb
@@ -14,10 +14,23 @@ File.open("/usr/share/dict/words").each_line do |line|
   end
 end
 
+# Dictionary = []
+# File.open("/usr/share/dict/words").each_line do |line|
+#   line.chomp! # remove \n from line
+#   if line.length >= 2 && line.length <= 7
+#     line.capitalize!
+#       if line.capitalize! == "nil"
+#         # line.delete
+#       else line.downcase
+#     end
+#     Dictionary.push line
+#   end
+# end
+
 def possible_words letters
   possibilities = []
-  combinations = letters.permutation.to_a + letters.permutation(2).to_a + letters.permutation(1).to_a
-  combinations.each  do  |x|
+  combinations = letters.permutation(7).to_a + letters.permutation(6).to_a + letters.permutation(5).to_a + letters.permutation(4).to_a + letters.permutation(3).to_a + letters.permutation(2).to_a
+  combinations.each do  |x|
     possibilities.push(x.join)
   end
   return possibilities
@@ -35,14 +48,15 @@ end
 
 class TestScrabble < Minitest::Test
   def test_possible_words
-    possibilites = possible_words(["c","a","t"])
+    possibilites = possible_words(["c","a","t", "m", "h", "s","e"])
     assert possibilites.include?("tca")
   end
   
   def test_is_string
-    found_words = word_finder(["c","a","t"])
-    assert found_words.length >= 5
+    found_words = word_finder(["c","a","t", "m", "h", "s","e"])
+    assert found_words.length >= 2
     assert found_words.include?("act")
+    assert !found_words.include?("matches")
     assert !found_words.include?("bat")
   end
 
@@ -50,4 +64,7 @@ class TestScrabble < Minitest::Test
     found_words = word_finder(["t","b","a"])
     assert found_words.include?("bat")
   end
+binding.pry
 end
+
+# 7 permutation, fix tests, get dictionary to remove proper nouns (words that start with capital letters)
